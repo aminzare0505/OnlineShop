f85cf8deb556f09b8fe31f5d8123b359280fe3ce
a.zare <Aminzare0505@gmail.com>
2025-08-02 15:59:22 +0330

    Add Migration and Update Database

---
+++ OnlineShop.Persistence/Migrations/20250802122412_FirstMigration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "pbl");
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                schema: "pbl",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Des = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreateDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category",
+                schema: "pbl");
+        }
+    }
+}
